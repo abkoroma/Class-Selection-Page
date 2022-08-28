074646c6afdd11649c49a921d448aae1191ec9c1
Abu Koroma <60013480+abkoroma@users.noreply.github.com>
2022-08-27 23:42:50 -0400

    Add files via upload

---
+++ api.js
@@ -0,0 +1,13 @@
+
+export const Api = (() => {
+    const baseUrl = "http://localhost:4232/courseList";
+
+    const getCourses = () => 
+        fetch(baseUrl).then(response => response.json());
+
+    return {
+        getCourses,
+    };
+})();
+
+
